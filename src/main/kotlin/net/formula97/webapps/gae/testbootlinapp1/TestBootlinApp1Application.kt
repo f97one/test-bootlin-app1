8dc4c3b101ee2ed453f382940070f94e529ba4b8
HAJIME Fukuna <f97one@gmail.com>
2019-10-05 13:15:57 +0000

    First commit

---
+++ src/main/kotlin/net/formula97/webapps/gae/testbootlinapp1/TestBootlinApp1Application.kt
@@ -0,0 +1,11 @@
+package net.formula97.webapps.gae.testbootlinapp1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestBootlinApp1Application
+
+fun main(args: Array<String>) {
+    runApplication<TestBootlinApp1Application>(*args)
+}
---
+++ src/test/kotlin/net/formula97/webapps/gae/testbootlinapp1/TestBootlinApp1ApplicationTests.kt
@@ -0,0 +1,16 @@
+package net.formula97.webapps.gae.testbootlinapp1
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TestBootlinApp1ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
